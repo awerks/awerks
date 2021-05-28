1dcbcc729b1a678b27374c652f82d9e793300428
awerks <sustila24@gmail.com>
2021-05-28 14:00:48 +0300

    rt
    r

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main()
+        {
+            
+            Console.OutputEncoding = Encoding.UTF8;
+            int x = 5;
+            Console.WriteLine($"'x' = {x}");
+            Console.ReadLine();
+
+
+        }
+    }
+    
+}
